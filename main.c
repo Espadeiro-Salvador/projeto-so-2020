ac9f9dc3e931bbc8d80e9179ac5a108a600d01c4
Eduardo Espadeiro <eduardo.espadeiro@tecnico.ulisboa.pt>
2020-10-15 20:22:50 +0100

    LIMPEZAAAAAA

--- main.c
+++ main.c
@@ -20,45 +20,36 @@ char inputCommands[MAX_COMMANDS][MAX_INPUT_SIZE];
 int numberCommands = 0;
 int headQueue = 0;
 
+/* Locks using mutex or wrlock. */
 void lockWriteFS() {
-    if (syncStrategy == MUTEX) {
-        if (pthread_mutex_lock(&mutex)) {
-            printf("Error: Mutex failed to lock\n");
-            exit(EXIT_FAILURE);
-        }
-    } else if (syncStrategy == RWLOCK) {
-        if (pthread_rwlock_wrlock(&rwlock)) {
-            printf("Error: RWLock failed to lock\n");
-            exit(EXIT_FAILURE);
-        }
+    if (syncStrategy == MUTEX && pthread_mutex_lock(&mutex)) {
+        printf("Error: Mutex failed to lock\n");
+        exit(EXIT_FAILURE);
+    } else if (syncStrategy == RWLOCK && pthread_rwlock_wrlock(&rwlock)) {
+        printf("Error: RWLock failed to lock\n");
+        exit(EXIT_FAILURE);
     }
 }
 
+/* Locks using mutex or rdlock. */
 void lockReadFS() {
-    if (syncStrategy == MUTEX) {
-        if (pthread_mutex_lock(&mutex)) {
-            printf("Error: Mutex failed to lock\n");
-            exit(EXIT_FAILURE);
-        }
-    } else if (syncStrategy == RWLOCK) {
-        if (pthread_rwlock_rdlock(&rwlock)) {
-            printf("Error: RWLock failed to lock\n");
-            exit(EXIT_FAILURE);   
-        }
+    if (syncStrategy == MUTEX && pthread_mutex_lock(&mutex)) {
+        printf("Error: Mutex failed to lock\n");
+        exit(EXIT_FAILURE);
+    } else if (syncStrategy == RWLOCK && pthread_rwlock_rdlock(&rwlock)) {
+        printf("Error: RWLock failed to lock\n");
+        exit(EXIT_FAILURE);   
     }
 }
 
+/* Unlocks the mutex or the rwlock. */
 void unlockFS() {
-    if (syncStrategy == MUTEX) {
-        if (pthread_mutex_unlock(&mutex)) {
-            printf("Error: Mutex failed to unlock\n");
-            exit(EXIT_FAILURE);
-        }
-    } else if (syncStrategy == RWLOCK) {
-        if (pthread_rwlock_unlock(&rwlock)) {
-            printf("Error: RWLock failed to unlock\n");
-            exit(EXIT_FAILURE);
-        }
+    if (syncStrategy == MUTEX && pthread_mutex_unlock(&mutex)) {
+        printf("Error: Mutex failed to unlock\n");
+        exit(EXIT_FAILURE);
+    } else if (syncStrategy == RWLOCK && pthread_rwlock_unlock(&rwlock)) {
+        printf("Error: RWLock failed to unlock\n");
+        exit(EXIT_FAILURE);
     }
 }
 
@@ -71,11 +62,9 @@ int insertCommand(char* data) {
 }
 
 char* removeCommand() {
-    if (syncStrategy != NOSYNC) {
-        if (pthread_mutex_lock(&mutex)) {
-            printf("Error: Mutex failed to lock\n");
-            exit(EXIT_FAILURE);
-        }
+    if (syncStrategy != NOSYNC && pthread_mutex_lock(&mutex)) {
+        printf("Error: Mutex failed to lock\n");
+        exit(EXIT_FAILURE);
     }
 
     if (numberCommands > 0) {
@@ -83,21 +72,17 @@ char* removeCommand() {
         numberCommands--;
         int head = headQueue++;
 
-        if (syncStrategy != NOSYNC) {
-            if (pthread_mutex_unlock(&mutex)) {
-                printf("Error: Mutex failed to unlock\n");
-                exit(EXIT_FAILURE);
-            }
+        if (syncStrategy != NOSYNC && pthread_mutex_unlock(&mutex)) {
+            printf("Error: Mutex failed to unlock\n");
+            exit(EXIT_FAILURE);
         }
 
         return inputCommands[head];
     }
 
-    if (syncStrategy != NOSYNC) {
-        if (pthread_mutex_unlock(&mutex)) {
-            printf("Error: Mutex failed to unlock\n");
-            exit(EXIT_FAILURE);
-        }
+    if (syncStrategy != NOSYNC && pthread_mutex_unlock(&mutex)) {
+        printf("Error: Mutex failed to unlock\n");
+        exit(EXIT_FAILURE);
     }
 
     return NULL;
@@ -224,6 +209,7 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
+    /* select synchronization strategy */
     if (!strcmp(argv[4], "mutex")) {
         syncStrategy = MUTEX;
     } else if (!strcmp(argv[4], "rwlock")) {
@@ -239,11 +225,9 @@ int main(int argc, char* argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    if (syncStrategy != NOSYNC) {
-        if (pthread_mutex_init(&mutex, NULL)) {
-            printf("Error: Mutex failed to initialize\n");
-            exit(EXIT_FAILURE);
-        }
+    if (syncStrategy != NOSYNC && pthread_mutex_init(&mutex, NULL)) {
+        printf("Error: Mutex failed to initialize\n");
+        exit(EXIT_FAILURE);
     }
     
     /* init filesystem */
@@ -258,20 +242,23 @@ int main(int argc, char* argv[]) {
 
     /* process input */
     processInput(inputFile);
+
+    /* close input file */
     if (fclose(inputFile)) {
         printf("Error: could not close the input file\n");
         exit(EXIT_FAILURE);
     }
 
-    /* create tasks pool */
     numberThreads = atoi(argv[3]);
     pthread_t tid[numberThreads];
     
+    /* get initial time */
     if (gettimeofday(&tv1, NULL) == -1) {
         printf("Error: could not get the time\n");
         exit(EXIT_FAILURE);
     }
 
+    /* create the tasks */
     for(int i = 0; i < numberThreads; i++) {
         if (pthread_create(&tid[i], NULL, threadFunction, NULL) != 0) {
             printf("Error: could not create thread\n");
@@ -279,6 +266,7 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    /* wait for all the threads to finish running */
     for (int i = 0; i < numberThreads; i++) {
         if (pthread_join(tid[i], NULL)) {
             printf("Error: error waiting for thread\n");
@@ -286,15 +274,19 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    /* get final time */
     if (gettimeofday(&tv2, NULL) == -1) {
         printf("Error: could not get the time\n");
         exit(EXIT_FAILURE);
     }
 
+    /* prints execution time */
     printf("TecnicoFS completed in %.4f seconds\n", 
         (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
         (double) (tv2.tv_sec - tv1.tv_sec));
 
+    
+    /* open output file */
     outputFile = fopen(argv[2], "w");
     if (!outputFile) {
         printf("Error: could not open output file\n");
@@ -303,6 +295,8 @@ int main(int argc, char* argv[]) {
 
     /* print tree */
     print_tecnicofs_tree(outputFile);
+
+    /* close output file */
     if (fclose(outputFile)) {
         printf("Error: could not close the output file\n");
         exit(EXIT_FAILURE);
@@ -310,19 +304,14 @@ int main(int argc, char* argv[]) {
 
     /* release allocated memory */
     destroy_fs();
-    if (syncStrategy != NOSYNC) {
-        if (pthread_mutex_destroy(&mutex)) {
-            printf("Error: could not destroy the mutex\n");
-            exit(EXIT_FAILURE);
-        }
-
-        if (syncStrategy == RWLOCK) {
-            if (pthread_rwlock_destroy(&rwlock)) {
-                printf("Error: could not destroy the rwlock\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-    }    
+    if (syncStrategy != NOSYNC && pthread_mutex_destroy(&mutex)) {
+        printf("Error: could not destroy the mutex\n");
+        exit(EXIT_FAILURE);
+    }
+    if (syncStrategy == RWLOCK && pthread_rwlock_destroy(&rwlock)) {
+        printf("Error: could not destroy the rwlock\n");
+        exit(EXIT_FAILURE);
+    }
 
     exit(EXIT_SUCCESS);
 }
